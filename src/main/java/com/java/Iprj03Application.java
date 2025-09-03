a111c2251b4587eb19b5d95e43e40b072486b81a
imhonggeun <dkfl9870@gmail.com>
2025-09-03 16:02:26 +0900

    처음 커밋

---
+++ src/main/java/com/java/Iprj03Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iprj03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iprj03Application.class, args);
+	}
+
+}
